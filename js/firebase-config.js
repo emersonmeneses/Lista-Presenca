d3e2e91700f8020620554c691b58acc7d2ee5ea1
EMERSON SANTOS - MENESES - JAVA <168465494+emersonmeneses@users.noreply.github.com>
2026-03-02 22:38:11 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
  const firebaseConfig = {
