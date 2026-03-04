b8d53d771910f053055de34d7707d4a2926ebaf6
EMERSON SANTOS - MENESES - JAVA <168465494+emersonmeneses@users.noreply.github.com>
2026-03-03 23:25:15 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,25 +2,42 @@ import { auth } from "./firebase-config.js";
 import { signInWithEmailAndPassword, sendPasswordResetEmail } 
 from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-
 document.addEventListener("DOMContentLoaded", () => {
 
-  const btnLogin = document.querySelector("button");
-  const emailInput = document.querySelectorAll("input")[0];
-  const senhaInput = document.querySelectorAll("input")[1];
-  const linkEsqueci = document.querySelector("a");
+  const btnLogin = document.getElementById("btnLogin");
+  const emailInput = document.getElementById("email");
+  const senhaInput = document.getElementById("senha");
+  const linkEsqueci = document.getElementById("esqueciSenha");
+  const text = btnLogin.querySelector(".btn-text");
+  const spinner = btnLogin.querySelector(".spinner");
 
   // LOGIN
   btnLogin.addEventListener("click", async () => {
+
     const email = emailInput.value;
     const senha = senhaInput.value;
 
+    if (!email || !senha) {
+      alert("Preencha email e senha.");
+      return;
+    }
+
+    // Mostra spinner
+    text.style.display = "none";
+    spinner.style.display = "block";
+    btnLogin.disabled = true;
+
     try {
       await signInWithEmailAndPassword(auth, email, senha);
-      alert("Login realizado com sucesso!");
-      window.location.href = "home.html"; // próxima tela
+      window.location.href = "painel.html";
     } catch (error) {
+
       alert("Erro: " + error.message);
+
+      // Volta botão ao normal
+      text.style.display = "inline";
+      spinner.style.display = "none";
+      btnLogin.disabled = false;
     }
   });
 
