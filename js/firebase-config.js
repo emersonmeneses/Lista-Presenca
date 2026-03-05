b39dffeb2c2a6504605b317c65cd76fcc1243e07
EMERSON SANTOS - MENESES - JAVA <168465494+emersonmeneses@users.noreply.github.com>
2026-03-04 22:32:47 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,15 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
- const firebaseConfig = {
-    apiKey: "AIzaSyCRwjKJyWu54ip_lnFK_2cIot0Zpnairnk",
-    authDomain: "lista-presenca-5f611.firebaseapp.com",
-    projectId: "lista-presenca-5f611",
-    storageBucket: "lista-presenca-5f611.firebasestorage.app",
-    messagingSenderId: "502952841131",
-    appId: "1:502952841131:web:bfc803e771ecc2bcd94cee",
-    measurementId: "G-JD3T2FHKBT"
-  };
+// Configuração do Firebase (COPIE DO SITE E COLE AQUI)
+const firebaseConfig = {
+  apiKey: "AIzaSyCRwjKJyWu54ip_lnFK_2cIot0Zpnairnk",
+  authDomain: "lista-presenca-5f611.firebaseapp.com",
+  projectId: "lista-presenca-5f611",
+  storageBucket: "lista-presenca-5f611.firebasestorage.app",
+  messagingSenderId: "502952841131",
+  appId: "1:502952841131:web:bfc803e771ecc2bcd94cee",
+  measurementId: "G-JD3T2FHKBT"
+};
 
+// Inicializa
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
